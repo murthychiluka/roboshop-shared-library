4a4af7925b6efde0356d026835fc310021013e22
Murthy <murthychiluka@gmail.com>
2023-04-04 22:05:32 +0530

    IF-Script

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,13 +4,17 @@ def call() {
     stages {
         stage ('compile/build'){
             steps {
-               if (env.app_lang == "nodejs") {
+                script {
+                    if (env.app_lang == "nodejs") {
                 sh 'npm install'
                }
                if (env.app_lang == "java") {
                 sh 'maven package'
                }
                 
+
+                }
+               
                 
             }
 
