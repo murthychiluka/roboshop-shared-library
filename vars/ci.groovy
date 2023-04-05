d5be63430a070719f360af9b1ab46a2aa04a533d
Murthy <murthychiluka@gmail.com>
2023-04-05 21:52:02 +0530

    vars/ci.groovy

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,41 +1,49 @@
 def call() {
-     if (!env.sonar_extra_opts) {
-          env.sonar_extra_opts=""
+     pipeline {
+        agent any
+        stages {
+            stage('compile/build') {
+                steps {
+                    script {
+                        common.compile()
+                    }
+          
+                }
+            }
+            stage ('test cases') {
+                steps {
+                    script {
+                        common.testcases()
+                    }
+                }
+
+            }
+             
+            stage ('common.codequality') {
+                steps {
+                    script {
+                        common.codequality()
+                    }
+                }
+ 
+                    
+            }
+        }
      }
+
+}
     
-    pipeline {
-    agent any
-    stages {
-        stage ('compile/build'){
-            steps {
-                script {
-                    common.compile()
-
-                }  
-            }
-        }
-
-        stage ('test cases'){
-            steps {
-                script {
-                    common.testcases()
-                }
-                
-            }
-        }
-        
-        stage ('code quality'){
-            steps {
-                script {
-                    common.codequality()
-                }
-                
-            }
-        }
-        
-
-    }
-}
-
-
-}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
