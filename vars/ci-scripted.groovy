37ef05c57b4e1e5e5b8fab27054313679aa416eb
Murthy <murthychiluka@gmail.com>
2023-04-09 15:41:13 +0530

    work

--- vars/ci-scripted.groovy
+++ vars/ci-scripted.groovy
@@ -2,17 +2,25 @@ def call() {
     if (!env.sonar_extra_opts) {
          env.sonar_extra_opts=""
     }
-    node ("work station") {
+    node("work station") {
         try {
+            stage('check out code') {
+                sh 'ls -l'
+                cleanWS()
+                sh 'ls -l'
+                git branch: 'main', url: 'https://github.com/murthychiluka/catalogue'
+                sh 'ls -l'
+            }
             stage('compile/build') {
+
                 sh 'env'
-               common.compile()
+                common.compile()
             }
         
-            stage ('test cases') {
+            stage('test cases') {
                common.testcases()
             }       
-            stage ('common.codequality') {
+            stage('common.codequality') {
                common.codequality()
             }
         } catch (e) {
