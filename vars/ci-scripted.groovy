684fa353910836f8d40d2fe004c8bfb0a3e84f6f
Murthy <murthychiluka@gmail.com>
2023-04-09 15:08:54 +0530

    try catch

---
+++ vars/ci-scripted.groovy
@@ -0,0 +1,45 @@
+def call() {
+    if (!env.sonar_extra_opts) {
+         env.sonar_extra_opts=""
+    }
+    node ("work station") {
+        try {
+            stage('compile/build') {
+               common.compile()
+            }
+        
+            stage ('test cases') {
+               common.testcases()
+            }       
+            stage ('common.codequality') {
+               common.codequality()
+            }
+        } catch (e){
+            mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'murthychiluka@gmail.com', subject: "${component} - Pipeline Failed", to: 'murthychiluka@gmail.com', mimeType: 'text/html' 
+          }
+    }
+}
+
+
+    
+
+
+    
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
