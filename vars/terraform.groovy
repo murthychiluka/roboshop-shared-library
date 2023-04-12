bf5adc47c6d1f8e6f44c6c9fabf588790299e06c
murthychiluka <murthychiluka@gmail.com>
2023-04-12 18:30:04 +0530

    D

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -2,21 +2,22 @@ def call() {
     pipeline {
         agent any
         parameters {
-            string(name: 'ENV', defaultValue: '', description: 'Which environment?')
-
+            string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+            string(name: 'ACTION', defaultValue: '', description: 'Which Action?')
+        }
+        options {
+            ansiColor('xterm')
         }
         
         stages {
             stage('Init') {
                 steps {
                     sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
-
                 }
             }
-
             stage('apply') {
                 steps {
-                    // sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars' 
+                    // sh 'terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars' 
                     sh 'echo'
                 }
             }
