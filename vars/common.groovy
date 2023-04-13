4ca138f130cd6c7e492d057b9e71c912ecd0ebc8
murthychiluka <murthychiluka@gmail.com>
2023-04-13 12:54:16 +0530

    d

--- vars/common.groovy
+++ vars/common.groovy
@@ -33,6 +33,7 @@ def prepareArtifacts() {
     sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
   }  else {
     sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
+    
   }
 
   
