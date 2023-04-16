f28f28d016f82b6c1e1127f0d11a4b3fef1d2301
murthychiluka <murthychiluka@gmail.com>
2023-04-17 01:47:39 +0530

    d

---
+++ vars/deploy.groovy
@@ -0,0 +1,41 @@
+def call() {
+    pipeline {
+        agent any
+        parameters {
+            string(name: 'app_version', defaultValue: '', description: 'App Version')
+            string(name: 'component', defaultValue: '', description: 'Component')
+            string(name: 'environment', defaultValue: '', description: 'Environment')
+        }
+
+      
+    
+        stages {
+            stage('Update Parameter Store') {
+                steps {
+                     sh 'aws ssm put-parameter --name ${environment}.${component}.app_version --type "String" --value "${app_version}" --overwrite'
+
+                }
+                
+            }
+            stage('Deploy Servers') {
+                steps {
+                     sh 'aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}-${environment}" --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >/tmp/servers'
+                     sh 'ansible-playbook -i /tmp/servers roboshop.yml -e role_name=${component} -e env=${environment} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD}'
+
+                }
+
+            }
+
+
+
+        }
+        post {
+            always {
+                cleanWs {
+
+                }
+            }
+        }
+    }
+
+}
