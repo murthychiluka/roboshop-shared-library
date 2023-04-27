7caf4c14d69dacfa3b9ec387cf849ca6e7506b81
murthychiluka <murthychiluka@gmail.com>
2023-04-27 21:46:53 +0530

    d

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -23,7 +23,7 @@ def call() {
             stage('Deploy Helm Chart') {
                 steps {
                     script {
-                        sh 'helm install ${component} APP/helm/${environment}.yaml'
+                        sh 'helm install ${component} . -f APP/helm/${environment}.yaml --set appversion=${app_version}'
                     }
                     
                     
